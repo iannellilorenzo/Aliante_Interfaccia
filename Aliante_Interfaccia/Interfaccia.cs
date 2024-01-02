1b0e37af5a97670df04efcb4f1f224494811b9a7
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-01-02 16:14:52 +0100

    file interfaccia creato e scritto

---
+++ Aliante_Interfaccia/Interfaccia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliante_Interfaccia
+{
+    interface IComposite
+    {
+        void Aggiunta(IComposite componente);
+
+        void Rimuovi(int index);
+
+        IComposite GetChild(int index);
+
+        bool Equals(object obj);
+
+        string ToString();
+
+        double Prezzo();
+    }
+}
