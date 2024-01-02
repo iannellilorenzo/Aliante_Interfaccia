d9ccb17220e1692ec644fa845c156e82380b2de2
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-01-02 16:31:38 +0100

    classi Gomma, Ruota e Aliante create e scritte

---
+++ Aliante_Interfaccia/Aliante.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliante_Interfaccia
+{
+    public class Aliante : IComposite
+    {
+        private List<IComposite> _components;
+
+        public List<IComposite> IComposites
+        {
+            get { return _components; }
+            set { _components = value; }
+        }
+
+        public Aliante()
+        {
+            IComposites = new List<IComposite>();
+        }
+
+        public Aliante(List<IComposite> components)
+        {
+            IComposites = components;
+        }
+
+        public Aliante(Aliante oldAliante)
+        {
+            IComposites = oldAliante.IComposites;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Aliante) || obj == null)
+            {
+                return false;
+            }
+
+            Aliante other = (Aliante)obj;
+            if (IComposites.Count != other.IComposites.Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < IComposites.Count; i++)
+            {
+                if (!IComposites[i].Equals(other.IComposites[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void Aggiunta(IComposite component)
+        {
+            IComposites.Add(component);
+        }
+
+        public void Rimuovi(int index)
+        {
+            IComposites.RemoveAt(index);
+        }
+
+        public IComposite GetChild(int index)
+        {
+            if (IComposites.Count > index)
+            {
+                return IComposites[index];
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+
+            foreach (var component in IComposites)
+            {
+                str += component.ToString();
+            }
+
+            return str;
+        }
+
+        public double Prezzo()
+        {
+            double tot = 0;
+
+            foreach (var component in IComposites)
+            {
+                tot += component.Prezzo();
+            }
+
+            return tot;
+        }
+    }
+}
---
+++ Aliante_Interfaccia/Gomma.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliante_Interfaccia
+{
+    public class Gomma : IComposite
+    {
+        private double _alt;
+        private double _larg;
+        private double _raggio;
+
+        public double Alt
+        {
+            get { return _alt; }
+            set
+            {
+                if (value >= 200F && value <= 400F)
+                    _alt = value;
+                else
+                    _alt = 300F;
+            }
+        }
+
+        public double Larg
+        {
+            get { return _larg; }
+            set
+            {
+                if (value >= 200F && value <= 400F)
+                    _larg = value;
+                else
+                    _larg = 300F;
+            }
+        }
+
+        public double Raggio
+        {
+            get { return _raggio; }
+            set
+            {
+                if (value >= 40F && value <= 60F)
+                    _raggio = value;
+                else
+                    _raggio = 50F;
+            }
+        }
+
+        public Gomma()
+        {
+            Alt = 0F;
+            Larg = 0F;
+            Raggio = 0F;
+        }
+
+        public Gomma(double alt, double larg, double raggio)
+        {
+            Alt = alt;
+            Larg = larg;
+            Raggio = raggio;
+        }
+
+        public Gomma(Gomma oldGomma)
+        {
+            Alt = oldGomma.Alt;
+            Larg = oldGomma.Larg;
+            Raggio = oldGomma.Raggio;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Gomma) || obj == null)
+            {
+                return false;
+            }
+
+            Gomma other = (Gomma)obj;
+            return Alt == other.Alt && Larg == other.Larg && Raggio == other.Raggio;
+        }
+
+        public void Aggiunta(IComposite component)
+        {
+
+        }
+
+        public void Rimuovi(int index)
+        {
+            
+        }
+
+        public IComposite GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Altezza: {Alt}; Larghezza: {Larg}; Raggio: {Raggio}";
+        }
+
+        public double Prezzo()
+        {
+            return Alt * Larg * Raggio * 10F;
+        }
+    }
+}
