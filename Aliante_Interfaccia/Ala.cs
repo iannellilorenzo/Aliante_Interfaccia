5b110d6c67d83bc65f84bfeba026684a02a6b213
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-01-02 16:20:22 +0100

    classe Ala creata e scritta

---
+++ Aliante_Interfaccia/Ala.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliante_Interfaccia
+{
+    public class Ala : IComposite
+    {
+        private double _lung;
+        private double _aper;
+
+        public double Lung
+        {
+            get { return _lung; }
+            set
+            {
+                if (value >= 15F)
+                    _lung = value;
+                else
+                    _lung = 15F;
+            }
+        }
+
+        public double Aper
+        {
+            get { return _aper; }
+            set
+            {
+                if (value * 2F >= Lung)
+                    _aper = value;
+                else
+                    _aper = Lung * 2F;
+            }
+        }
+
+        public Ala()
+        {
+            Lung = 0F;
+            Aper = 0F;
+        }
+
+        public Ala(double lung, double aper)
+        {
+            Lung = lung;
+            Aper = aper;
+        }
+
+        public Ala(Ala oldWing)
+        {
+            Lung = oldWing.Lung;
+            Aper = oldWing.Aper;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Ala) || obj == null)
+            {
+                return false;
+            }
+
+            Ala other = (Ala)obj;
+            return Lung == other.Lung && Aper == other.Aper;
+        }
+
+        public void Aggiunta(IComposite component)
+        {
+
+        }
+
+        public void Rimuovi(int index)
+        {
+            
+        }
+
+        public IComposite GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Lunghezza ala: {Lung}; Apertura ala: {Aper}";
+        }
+
+        public double Prezzo()
+        {
+            return Lung * Aper;
+        }
+    }
+}
